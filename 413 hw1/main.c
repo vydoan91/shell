36ba521c93f602c4b076b2db9a233b352ea4a477
vydoan91 <vy.h.doan@gmail.com>
2019-02-06 22:32:44 -0800

    Initial Commit

---
+++ 413 hw1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  413 hw1
+//
+//  Created by VY DOAN on 2/6/19.
+//  Copyright © 2019 VY DOAN. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
